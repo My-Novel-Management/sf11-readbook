0b455e9e7274f95df3e4a40447a597e58359fa1f
N.T.WORKS <nagisc007@yahoo.co.jp>
2020-08-26 10:10:40 +0900

    Up sb (#11)
    * Update storybuilder (v0.6.1)
    
    * Fix minor
    
    * Changed for sb-v0.6
    
    * Fix detail

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,11 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+## [2.1.0] - 2020-08-25
+### Changed
+- StoryBuilder: v0.6.1
+- main: for sb-v0.6
+
 ## [2.0.0] - 2020-04-09
 ### Fixed
 - detail: production
@@ -31,7 +36,8 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - This CHANGELOG file to hopefully serve as an evolving example of a standardized open source project CHANGELOG.
 - README one line implemented.
 
-[Unreleased]: https://github.com/My-Novel-Management/sf11-readbook/compare/v2.0.0...HEAD
+[Unreleased]: https://github.com/My-Novel-Management/sf11-readbook/compare/v2.1.0...HEAD
+[2.1.0]: https://github.com/My-Novel-Management/sf11-readbook/releases/v2.1.0
 [2.0.0]: https://github.com/My-Novel-Management/sf11-readbook/releases/v2.0.0
 [1.2.0]: https://github.com/My-Novel-Management/sf11-readbook/releases/v1.2.0
 [1.1.0]: https://github.com/My-Novel-Management/sf11-readbook/releases/v1.1.0
--- src/main.py
+++ src/main.py
@@ -1,73 +1,89 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""Main story.
-"""
-## path setting
+'''
+Story: "title"
+'''
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append('storybuilder')
-## public libs
-## local libs
 from storybuilder.builder.world import World
-from storybuilder.builder.writer import Writer
-## assets
-from storybuilder.assets import basic, accessory
-## settings
-from config import PERSONS, AREAS, STAGES, DAYS, TIMES, ITEMS, WORDS, RUBIS, LAYERS
-## local files
-from episodes.e1_ghostworld import ep_ghostworld
-from episodes.e2_oldlibrary import ep_old_library
-from episodes.e3_waitinggirl import ep_waiting_girl
-from episodes.e4_truth import ep_truth
+from storybuilder.assets import basic
+from storybuilder.assets import common_rubi
+from config import ASSET
+# import scenes
+# from scenes import xxx
+from episodes import e1_ghostworld
+from episodes import e2_oldlibrary
+from episodes import e3_waitinggirl
+from episodes import e4_truth
 
-
-## define alias
-W = Writer
-_ = Writer.getWho()
 
 ################################################################
 #
-#   1. 滅びた世界
-#   2. 図書館の女性
-#   3. 待つこと
-#   4. 記録士の仕事
+#   1. Initialize
+#   2. Story memo
+#   3. Structure    - 1/8
+#   4. Spec
+#   5. Plot         - 1/4
+#   6. Scenes
+#   7. Conte        - 1/2
+#   8. Layout
+#   9. Draft        - 1/1
 #
 ################################################################
 
+# Constant
+TITLE = "滅びた世界で、本を読む"
+MAJOR, MINOR, MICRO = 2, 1, 0
+COPY = "全てが滅びた世界でも、人は本を読む"
+ONELINE = "約18000字のSF短編。全てが滅んだ世界を記録する記録士と相棒のロボ犬の物語"
+OUTLINE = "すべてが滅んだ世界で、それでもまだ人類が生き延びる為に調査をしている「記録士」と呼ばれる存在がいた。その一人であるユーリィは相棒の記録用犬型ドローン「クド」と共に未登録地区を探して旅をしていた。その最中、本が残っている図書館を発見する"
+THEME = "本を読む、という行為"
+GENRE = "SF／ヒューマンドラマ"
+TARGET = "20-40years"
+SIZE = "電撃サイズで50枚"
+CONTEST_INFO = "創元SF短編賞一次突破作品を改稿し、電撃小説大賞に応募。一次落選"
+CAUTION = ""
+NOTE = "元々のオリジナルはエブリスタの妄想コンテストに応募した8000字のSF短編です"
+SITES = ["エブリスタ", "小説家になろう", "ノベルアッププラス", "カクヨム"]
+TAGS = ["SF", "ロボット", "犬", "本", "記録", "図書館"]
+RELEASED = (8, 25, 2020)
 
-## main
+
+# Chapters
 def ch_main(w: World):
     return w.chapter("main",
-            ep_ghostworld(w),
-            ep_old_library(w),
-            ep_waiting_girl(w),
-            ep_truth(w),
+            e1_ghostworld.ep_ghostworld(w),
+            e2_oldlibrary.ep_old_library(w),
+            e3_waitinggirl.ep_waiting_girl(w),
+            e4_truth.ep_truth(w),
             )
 
-def create_world():
-    """Create a world.
-    """
-    w = World("滅びた世界で、本を読む")
-    w.setCommonData()
-    w.setAssets(basic.ASSET)
-    w.setAssets(accessory.ASSET)
-    w.buildDB(PERSONS,
-            AREAS, STAGES, DAYS, TIMES, ITEMS, WORDS,
-            RUBIS, LAYERS)
-    w.setBaseDate(2108)
-    w.setBaseArea("Jakarta")
-    w.setColumnRow(42,34)
-    # set textures
-    # w.entryBlock()
-    # w.entryHistory()
-    # w.entryLifeNote()
-    w.setOutline("滅びた世界で、調査していた")
-    return w
 
-
+# Main
+# NOTE: 電撃サイズは42x34
 def main(): # pragma: no cover
-    w = create_world()
-    return w.build(
+    w = World.create_world(f"{TITLE}")
+    w.config.set_version(MAJOR, MINOR, MICRO)
+    w.db.set_from_asset(basic.ASSET)
+    w.db.set_from_asset(common_rubi.ASSET)
+    w.db.set_from_asset(ASSET)
+    # spec
+    w.config.set_copy(f"{COPY}")
+    w.config.set_oneline(f"{ONELINE}")
+    w.config.set_outline(f"{OUTLINE}")
+    w.config.set_theme(f"{THEME}")
+    w.config.set_genre(f"{GENRE}")
+    w.config.set_target(f"{TARGET}")
+    w.config.set_size(f"{SIZE}")
+    w.config.set_contest_info(f"{CONTEST_INFO}")
+    w.config.set_caution(f"{CAUTION}")
+    w.config.set_note(f"{NOTE}")
+    w.config.set_sites(*SITES)
+    w.config.set_taginfos(*TAGS)
+    w.config.set_released(*RELEASED)
+    return w.run(
             ch_main(w),
             )
 
